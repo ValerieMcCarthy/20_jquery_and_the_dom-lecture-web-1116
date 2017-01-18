85eb985e3c3397944fc997007eb401438c11b546
ipc103 <iancandysemail@gmail.com>
2017-01-18 12:01:18 -0500

    refactored

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-$(document).ready(function(){
-  $('form#todo-form').submit(function(event){
-    // stop the default action
-    event.preventDefault()
-    // find what the user inputted
-    var $input = $('input#todo')
-    var newTodoItem = $input.val()
-    if (newTodoItem.length > 0 ) {
-      $('ul#todos').append(`<li class='collection-item'>${newTodoItem}</li>`)
-      // clear the input
-      $input.val('')
-    }
-    // append that to the ul in the DOM...
+$( addFormEventHandler )
+// $(document).ready(addFormEventHandler)
+//  $(document).on('ready', addFormEventHandler)
+// addFormEventHandler will get called whenever the DOM is ready
 
-  })
-})
+function addFormEventHandler () {
+  $('form#todo-form').submit( handleFormSubmit )
+}
+
+function handleFormSubmit (event) {
+  event.preventDefault()
+  var newTodoItem =  getUserInput()
+  appendListItem(newTodoItem)
+}
+
+function getUserInput(){
+  var $input = $('input#todo')
+  var newTodoItem =  $input.val()
+  $input.val('')
+  return newTodoItem
+}
+
+function appendListItem(newTodoItem){
+  if (newTodoItem.length > 0 ) {
+    $('ul#todos').append(`<li class='collection-item'>${newTodoItem}</li>`)
+  }
+}
